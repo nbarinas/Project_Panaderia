c630598e68335ad76cd97e32373edc3fab751646
nbarinas <nbarinas@gmail>
2024-04-25 20:47:17 -0500

    cambio link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './NavBar';
+import Home from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div>
+        <Navbar /> {/* Mostrar el componente Navbar en todas las páginas */}
+        <Routes>
+          <Route exact path="/" element={<Home />} /> {/* Página de inicio con solo un botón */}
+          {/* Aquí puedes agregar más rutas */}
+        </Routes>
+        <Footer /> {/* Mostrar el componente Footer en todas las páginas */}
+      </div>
+    </Router>
   );
 }
 
