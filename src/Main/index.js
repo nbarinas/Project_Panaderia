d9d1c08a7d9695dc23c8a91836c90739e7c7f916
Jdmedina08 <jdmedina08@hotmail.es>
2024-04-26 21:29:39 -0500

    Navbar y carrusel agregados

--- src/Main/index.js
+++ src/Main/index.js
@@ -3,7 +3,42 @@ import React from 'react';
 const Main = () => {
   return (
     <div>
-      <h1>Aqui el Main.</h1>
+      <h1>Pagina de inicio</h1>
+      <div
+        id="carouselExampleFade"
+        class="carousel slide carousel-fade"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="mae-mu.jpg" class="d-block w-100" alt="panaderia" />
+          </div>
+          <div class="carousel-item">
+            <img src="nadya.jpg" class="d-block w-80" alt="panadero" />
+          </div>
+          <div class="carousel-item">
+            <img src="yeh.jpg" class="d-block w-80" alt="panaderia 2" />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
   );
 }
