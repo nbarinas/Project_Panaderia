ee098ac0961058e45650fa80a0021ea2aa896361
Jdmedina08 <jdmedina08@hotmail.es>
2024-05-03 19:48:54 -0500

    Modulo de sedes agregado

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './NavBar';
 import Carousel from './Carousel';
 import Footer from './Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Sedes from './Sedes';
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <div>
         <Navbar /> {/* Mostrar el componente Navbar en todas las páginas */}
         <Routes>
-          <Route exact path="/" element={<Carousel />} /> {/* Página de inicio con solo un botón */}
+          <Route exact path="/" element={<Sedes />} /> {/* Página de inicio con solo un botón */}
 
           {/* Aquí puedes agregar más rutas */}
         </Routes>
