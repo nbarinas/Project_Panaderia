9644d6b8c2e3015e7fb4eb5a14f98746d158c3c6
ANDRES TORRES <andrus72141@gmail.com>
2024-05-08 23:00:31 -0500

    iconos desplazables

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -5,15 +5,24 @@ const Footer = () => {
     <div>
       <footer className=" text-center text-lg-start text-muted bg-warning">
         {/* Section: Social media */}
-        <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom icon-redes " >
           {/* Left */}
           <div className="me-5 d-none d-lg-block fw-bold fs-4">
-            <span>Conectate con nuestras redes sociales:</span>
+            <span>SIGUENOS:</span>
           </div>
           {/* Left */}
           {/* Right */}
           <div>
-            <a href="" className="me-4 text-reset ">
+            <a  href="" className="me-5 text-reset  bottom-100px ">
+              <i className="fa fa-whatsapp-w fa-5x" >
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-whatsapp" viewBox="0 0 16 16">
+              <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"/>
+            </svg>
+              </i>
+              
+
+            </a>
+            <a href="" className="me-4 text-reset  ">
               <i className="fab fa-facebook-f">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -21,13 +30,12 @@ const Footer = () => {
                   height="25"
                   fill="currentColor"
                   class="bi bi-facebook"
-                  viewBox="0 0 16 16"
-                >
+                  viewBox="0 0 16 16">
                   <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
                 </svg>
               </i>
             </a>
-            <a href="" className="me-4 text-reset">
+            <a href="" className="me-4 text-reset ">
               <i className="fab fa-twitter">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -41,7 +49,7 @@ const Footer = () => {
                 </svg>
               </i>
             </a>
-            <a href="" className="me-4 text-reset">
+            <a href="" className="me-4 text-reset ">
               <i className="fab fa-google">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -55,8 +63,8 @@ const Footer = () => {
                 </svg>
               </i>
             </a>
-            <a href="" className="me-4 text-reset">
-              <i className="fab fa-instagram">
+            <a href="" className="me-4 text-reset " >
+              <i className="fab fa-instagram ">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="25"
@@ -69,7 +77,7 @@ const Footer = () => {
                 </svg>
               </i>
             </a>
-            <a href="" className="me-4 text-reset">
+            <a href="" className="me-4 text-reset ">
               <i className="fab fa-linkedin">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -83,7 +91,7 @@ const Footer = () => {
                 </svg>
               </i>
             </a>
-            <a href="" className="me-4 text-reset">
+            <a href="" className="me-4 text-reset ">
               <i className="fab fa-github">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
