e8bfa9f439da6210eae9bc82e9a0d1bb117909c0
nbarinas <nbarinas@gmail>
2024-05-26 10:20:54 -0500

    ahora suma el tu order

--- src/MyOrder/index.js
+++ src/MyOrder/index.js
@@ -1,10 +1,38 @@
-import React, { useState } from "react";
-import Layout from "../Layout";
+import React from 'react';
+import { useCart } from '../CartContext'; // Importa el contexto del carrito
+
 const MyOrder = () => {
+  const { cartItems } = useCart(); // Obtiene los elementos del carrito del contexto
+
+  // Función para calcular el total a pagar
+  const calcularTotal = () => {
+    return cartItems.reduce((total, item) => total + item.price, 0);
+  };
+
+  // Verifica si no hay elementos en el carrito y muestra un mensaje
+  if (cartItems.length === 0) {
+    return <div style={{ marginTop: '10px', textAlign: 'center' }}>No tienes nada agregado a tu orden.</div>;
+  }
+
   return (
-    <Layout>
-      MI ORDEN
-    </Layout>
+    <div style={{ marginTop: '80px' }}>
+      <h2 style={{ marginBottom: '10px' }}>Tu Orden</h2>
+      <div className="flex flex-col gap-4"> {/* Utiliza flexbox para mostrar los elementos uno encima del otro */}
+        {cartItems.map((item, index) => (
+          <div key={index} className="flex items-center gap-2"> {/* Cada artículo en un contenedor flex */}
+            <div style={{ marginLeft: '10%' }}> {/* Mueve los elementos un poco más hacia la derecha */}
+              <img src={item.image} alt={item.title} className="h-10 w-10 object-cover rounded-md" />
+            </div>
+            <div>{item.title}</div> {/* Nombre del artículo */}
+            <div>${item.price}</div> {/* Precio del artículo */}
+          </div>
+        ))}
+      </div>
+      <div style={{ marginTop: '20px', textAlign: 'right', marginRight: '10%' }}>
+        Total a Pagar: ${calcularTotal()}
+      </div>
+    </div>
   );
 };
+
 export default MyOrder;
